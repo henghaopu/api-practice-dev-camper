64c0eea4ffb36016394f333782b63de581fd918a
Heng-Hao Pu <hpu@oswego.edu>
2020-05-20 00:03:14 -0400

    Create a logger middleware
     - typically we won't put middleware right in server.js
     - we want to keep server.js as clean as possible

--- server.js
+++ server.js
@@ -9,6 +9,21 @@ dotenv.config({ path: './config/config.env' });
 
 // Initialize the app variable with express
 const app = express();
+
+// Create middleware
+const logger = (req, res, next) => {
+  // set a value on req obj, then we can access in any route that come after this middleware
+  // req.hello = 'Hello World!';
+  console.log(
+    `${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`
+  );
+  // we need to call next() in every middleware we create, so it can move on to the next middleware
+  next();
+};
+
+// Use middleware
+app.use(logger);
+
 // Mount/connect routers to their own specific url
 app.use('/api/v1/bootcamps', bootcamps);
 
